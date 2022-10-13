4ad0be7cbaf9911f096345db06f2e7230e0dcabe
wladimov <info@wladimirmoya.com>
2022-10-13 15:30:11 -0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>React Shop</h1>
+            <a href="/">Home</a>
+            <a href="/login">Login</a>
+            <a href="/recovery-password">Recovery</a>
+        </div>
+    );
+}
+
+export default Home;
