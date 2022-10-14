20caa367d6e75ca30ede95760d29bce9baea2734
wladimov <info@wladimirmoya.com>
2022-10-14 14:37:13 -0500

    [ADD] pages-routes-components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import ProductList from "../containers/ProductList";
 
 const Home = () => {
     return (
-        <div>
-            <h1>React Shop</h1>
-            <a href="/">Home</a>
-            <a href="/login">Login</a>
-            <a href="/recovery-password">Recovery</a>
-        </div>
+        <>
+            <ProductList />
+        </>
     );
 }
 
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
 import Home from '../pages/Home';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
 
 import '../styles/global.css';
+
 
 const App =() => {
     return (
@@ -15,7 +22,13 @@ const App =() => {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Login} />
-                    <Route exact path="/recovery-password" component={RecoveryPassword} />
+                    <Route exact path="/password-recovery" component={PasswordRecovery} />
+                    <Route exact path="/send-email" component={SendEmail} />
+                    <Route exact path="/new-password" component={NewPassword} />
+                    <Route exact path="/account" component={MyAccount} />
+                    <Route exact path="/singup" component={CreateAccount} />
+                    <Route exact path="/checkout" component={Checkout} />
+                    <Route exact path="/orders" component={Orders} />
                     <Route path="*" component={NotFound} />
                 </Switch>
             </Layout>
